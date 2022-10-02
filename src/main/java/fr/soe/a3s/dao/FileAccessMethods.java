0ca6480193f460dd56a9ad337d8072289a75e44c
Schwaggot <tom.ryan@posteo.de>
2022-10-02 13:31:43 +0200

    Removed non UTF8 characters

--- src/main/java/fr/soe/a3s/dao/FileAccessMethods.java
+++ src/main/java/fr/soe/a3s/dao/FileAccessMethods.java
@@ -130,36 +130,23 @@ public class FileAccessMethods implements DataAccessConstants {
 
 	public static void extractToFolder(File zipFile, File folder) throws IOException {
 
-		// création de la ZipInputStream qui va servir à lire les données du
-		// fichier zip
 		ZipInputStream zis = new ZipInputStream(
 				new BufferedInputStream(new FileInputStream(zipFile.getCanonicalFile())));
 
-		// extractions des entrées du fichiers zip (i.e. le contenu du zip)
 		ZipEntry ze = null;
 		try {
 			while ((ze = zis.getNextEntry()) != null) {
 
-				// Pour chaque entrée, on crée un fichier
-				// dans le répertoire de sortie "folder"
 				File f = new File(folder.getCanonicalPath(), ze.getName());
 
-				// Si l'entrée est un répertoire,
-				// on le crée dans le répertoire de sortie
-				// et on passe à l'entrée suivante (continue)
 				if (ze.isDirectory()) {
 					f.mkdirs();
 					continue;
 				}
 
-				// L'entrée est un fichier, on crée une OutputStream
-				// pour écrire le contenu du nouveau fichier
 				f.getParentFile().mkdirs();
 				OutputStream fos = new BufferedOutputStream(new FileOutputStream(f));
 
-				// On écrit le contenu du nouveau fichier
-				// qu'on lit à partir de la ZipInputStream
-				// au moyen d'un buffer (byte[])
 				try {
 					try {
 						final byte[] buf = new byte[8192];
@@ -170,13 +157,11 @@ public class FileAccessMethods implements DataAccessConstants {
 						fos.close();
 					}
 				} catch (final IOException ioe) {
-					// en cas d'erreur on efface le fichier
 					f.delete();
 					throw ioe;
 				}
 			}
 		} finally {
-			// fermeture de la ZipInputStream
 			zis.close();
 		}
 	}
